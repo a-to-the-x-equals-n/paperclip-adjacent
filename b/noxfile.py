f87aad5086e03ed9d02c242758341f43a0edb30e
Logan Reine <log.a.of.n@gmail.com>
2025-04-03 12:28:57 -0400

    reworked pyproject and nox to accomodate jenkins CI and auto build

--- b/noxfile.py
+++ b/noxfile.py
@@ -2,38 +2,53 @@ import nox
 from pathlib import Path
 
 # --- GLOBAL CONFIG ---
+
 HOST = 'localhost'
 PORT = '8007'
-BASE = f'http://localhost:8007'
+BASE = f'http://{HOST}:{PORT}'
+DIR = Path("b/tests/results")
+
+
+def _prepare_env(session):
+    session.install(".[dev]")
+    root = Path(__file__).parent.resolve()
+    session.env["PYTHONPATH"] = str(root)
+    DIR.mkdir(parents = True, exist_ok = True)
+
 
 @nox.session
 def test(session):
     '''
-    Run all pytest tests.
+    Run all tests and export results as JUnit XML (for Jenkins).
     '''
-    session.install(".[dev]")
-    # Ensure PYTHONPATH includes project root so App/ is importable
-    root = Path(__file__).parent.resolve()
-    session.env["PYTHONPATH"] = str(root)
-    session.run("pytest", "tests")
+    _prepare_env(session)
+    
+    # use pytest config from pyproject.toml
+    session.run("pytest")
+
 
 @nox.session
 def test_memcell(session):
-    session.install(".[dev]")
-    root = Path(__file__).parent.resolve()
-    session.env["PYTHONPATH"] = str(root)
+    '''
+    Run only memcell tests.
+    '''
+    _prepare_env(session)
     session.run("pytest", "tests/test_memcell.py")
+
 
 @nox.session
 def test_yamel(session):
-    session.install(".[dev]")
-    root = Path(__file__).parent.resolve()
-    session.env["PYTHONPATH"] = str(root)
+    '''
+    Run only Yamel tests.
+    '''
+    _prepare_env(session)
     session.run("pytest", "tests/test_yamel.py")
+
 
 @nox.session
 def test_routes(session):
-    session.install(".[dev]")
-    root = Path(__file__).parent.resolve()
-    session.env["PYTHONPATH"] = str(root)
+    '''
+    Run only integration tests for Flask routes.
+    '''
+    _prepare_env(session)
     session.run("pytest", "tests/test_routes.py")
